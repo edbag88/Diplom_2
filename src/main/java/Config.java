dd881a70a850dfeac02bea1cce78300fe9893f19
Eduard Bagautdinov <edbag88@gmail.com>
2023-03-28 19:28:46 +0500

    add Config file

---
+++ src/main/java/Config.java
@@ -0,0 +1,6 @@
+public class Config {
+    private static final String BASE_URI = "https://stellarburgers.nomoreparties.site/api";
+    public static String getBaseUri(){
+        return BASE_URI;
+    }
+}
